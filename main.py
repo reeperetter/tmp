c6dd8412c9f46b774cb92182d8d43459dbee2e5e
reeperetter <reeperetter@gmail.com>
2025-12-18 16:47:11 +0200

    85 page of documentation...

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from kivy.uix.actionbar import Button
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+def my_callback(dt):
+    print("Working!!!")
 
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
@@ -14,9 +22,26 @@ class LoginScreen(GridLayout):
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
 
+    event = Clock.schedule_once(my_callback, 3)
+    trigger = Clock.create_trigger(my_callback)
+    trigger()
+
+
+class RootWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        btn = Button(text="Button 1")
+        btn.bind()
+        self.add_widget(btn)
+        self.add_widget(Button(text="Button 2"))
+    
+    def btn_pressed(self, instance, pos):
+        print('pos: printed from root widget: {pos}'.format(pos=.pos))
+
+
 class MyApp(App):
     def build(self):
-        return LoginScreen()
+        return RootWidget()
 
 
 if __name__ == '__main__':

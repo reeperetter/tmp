3bd2bcdf3acc7a32d61ff688b81718ec3693cd9f
reeperetter <reeperetter@gmail.com>
2025-12-24 10:14:31 +0200

    base app

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,19 @@
-Pong
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+
+
+class MainScreen(BoxLayout):
+    status_text = StringProperty("Очікування...")
+
+    def update_status(self, btn_text):
+        self.status_text = f"Натиснуто: {btn_text}"
+
+
+class SuperApp(App):
+    def build(self):
+        return MainScreen()
+
+
+if __name__ == "__main__":
+    SuperApp().run()
